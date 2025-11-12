864ddfae6b7878b8aefc6291d510678e419b01b3
robemorin <robertomorin2@gmail.com>
2025-11-12 08:36:42 -0600

    25-11-12

--- src/tlacuache-modulo.mjs
+++ src/tlacuache-modulo.mjs
@@ -259,6 +259,45 @@ export const stat={
         const n = arr1Copy.length;
         const spearmanCorrelation = 1 - (6 * dSquared) / (n * (n * n - 1));
         return spearmanCorrelation;
+    },
+    calc_2varStat(x,y){
+        /*Emula la función 2 Var Stats de la calculadora*/
+        /*var x=[-21,-17,-15,-14,-13]
+        var y=[0,1,2,4,5]*/
+        
+        var n=x.length
+        var xm=this.mean(x)
+        var ym=this.mean(y)
+        var Sumx=0
+        var Sumy=0
+        var Sumx2=0
+        var Sumy2=0
+        var Sumxy=0
+        var Sx=0
+        var Sy=0;
+        for(var k=0;k<n;++k){
+            Sumx+=x[k]
+            Sumy+=y[k]
+            Sumx2+=x[k]*x[k]
+            Sumy2+=y[k]*y[k]
+            Sumxy+=x[k]*y[k]
+            Sx+=Math.pow(x[k]-xm,2)
+            Sy+=Math.pow(y[k]-ym,2)
+        }
+        var S="<center><table><tr><td style='background-color:#333;color:white'> <b>2 Var Stats</b></td></tr>"
+        S+="<tr><td align='left'>$\\overline x = "+Sumx/n+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sum x = "+Sumx+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sum x^2 = "+Sumx2+"$</td></tr>"
+        S+="<tr><td align='left'>$S_x= "+Math.sqrt(Sx/(n-1))+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sigma_x="+Math.sqrt(Sx/n)+"$</td></tr>"
+        S+="<tr><td align='left'>$n="+n+"$</td></tr>"
+        S+="<tr><td align='left'>$\\overline y = "+Sumy/n+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sum y = "+Sumy+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sum y^2 = "+Sumy2+"$</td></tr>"
+        S+="<tr><td align='left'>$S_y= "+Math.sqrt(Sy/(n-1))+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sigma_y="+Math.sqrt(Sy/n)+"$</td></tr>"
+        S+="<tr><td align='left'>$\\sum xy="+Sumxy+"$</td></tr>"
+        return S
     }
 
     //-------------------------
